7a1f818f668a1d3204b095fbb4fbd65313bcbe28
Norppa <62590151+NorppaVtuber@users.noreply.github.com>
2025-01-26 12:43:54 +0200

    Let's hope and pray there is no merge conflict

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     [Header("UI stuff")]
     [SerializeField] List<GameObject> healthBubbles;
+    [SerializeField] GameObject deathMessageObject;
 
     [Header("PlayerStuffs")]
     [SerializeField] PlayerMovement playerMovement;
@@ -23,31 +25,52 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         playerHealth.OnDamageTaken.AddListener(updateUI);
+        deathMessageObject.SetActive(false);
     }
 
-    public PlayerMovement GetPlayerMovement() {  return playerMovement; }
+    public PlayerMovement GetPlayerMovement() { return playerMovement; }
     public PlayerHealth GetPlayerHealth() { return playerHealth; }
     public Gun GetPlayerGun() { return gun; }
     public EnemyMovement GetEnemyMovement(GameObject _enemy)  //TODO: go through the Dictionary and find the correct script to reference in these two thingamajigs
     {
         EnemyMovement _neededMovement = _enemy.GetComponent<EnemyMovement>();
-        return _neededMovement; 
+        return _neededMovement;
     }
-    public EnemyHealth GetEnemyHealth(GameObject _enemy) 
+    public EnemyHealth GetEnemyHealth(GameObject _enemy)
     {
         EnemyHealth _neededEnemyHealth = _enemy.GetComponent<EnemyHealth>();
-        return _neededEnemyHealth; 
+        return _neededEnemyHealth;
+    }
+
+    public void ResetButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     void updateUI()
     {
         for (int i = 0; i < healthBubbles.Count; i++) //I could reverse this, or I could just shove the bubbles in the list in a reverse order
         {
-            if(healthBubbles[i].activeSelf == true)
+            if (healthBubbles[i].activeSelf == true)
             {
                 healthBubbles[i].SetActive(false);
+
+                if (i == healthBubbles.Count - 1)
+                {
+                    sendDeathMessage();
+                }
                 return;
             }
         }
+    }
+
+    void sendDeathMessage()
+    {
+        deathMessageObject.SetActive(true);
     }
 }
